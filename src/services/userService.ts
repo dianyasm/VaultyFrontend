ee9cbc9925ce1d7d9f599d52ee1a5da54b2e9ebd
dianyasm <dianyasm@gmail.com>
2025-02-11 14:06:36 +0100

    correcion de api

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,7 +1,9 @@
-const URL_BASE = 'http://localhost:3000/api/'
+//const URL_BASE = 'http://localhost:3000/api/'
+const API_URL_BASE = import.meta.env.VITE_API_URL_BASE
+
 export const getUsers = async () => {
     try{
-        const response = await fetch(URL_BASE + 'users/',{
+        const response = await fetch(API_URL_BASE + '/users/',{
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -9,7 +11,7 @@ export const getUsers = async () => {
             credentials: 'include'
         })
         if(!response.ok){
-            throw new Error('fallo al obtener los usuario')
+            throw new Error('fallo al obtener los usuarioS')
         }
         return await response.json()
     }catch(error){
