a6f901978ee413e893b13024e5b9dba16364234c
dianyasm <dianyasm@gmail.com>
2025-02-13 09:54:30 +0100

    intento de soluciones

--- src/utils/FetchAPI.ts
+++ src/utils/FetchAPI.ts
@@ -1,34 +1,35 @@
 export const fetchAPI = async (endPoint: string, options = {}) => {
     try {
-        const response = await fetch(endPoint, options)
-
-        if (response.status == 401) {
-            window.location.href = '/login'
-            throw new Error('Sesión expirada. Inicia sesión nuevamente')
+      const response = await fetch(endPoint, options);
+  
+      if (response.status == 401) {
+        window.location.href = "/login";
+        throw new Error("Sesión expirada. Inicia sesión nuevamente");
+      }
+  
+      /*  if (!response.ok) {
+              const errorData = await response.json().catch(() => null)
+              throw new Error(errorData?.message || 'Error desconocido')
+          } */
+  
+      const jsonData = await response.json();
+      if (!response.ok) {
+        if (jsonData.error) {
+          throw jsonData.error;
+        } else if (jsonData.message) {
+          throw jsonData.message;
+        } else {
+          throw { error: jsonData };
         }
-
-        /*if (!response.ok) {
-            const errorData = await response.json().catch(() => null)
-            throw new Error(errorData?.message || 'Error desconocido')
-        }*/
-        const jsonData = await response.json();
-        if (!response.ok) {
-            if (jsonData.error) {
-                throw jsonData.error;
-            } else if (jsonData.message) {
-                throw jsonData.message;
-            } else {
-                throw { error: jsonData };
-            }
-        }
-
-        return jsonData
+      }
+  
+      return jsonData;
     } catch (error) {
-        /*} catch (error) {
-            const msg = error instanceof Error ? error.message : 'Error desconocido'
-            throw new Error(msg)
-        }*/
-        console.error("error:", error)
-        throw error
+      /* catch (error) {
+          const msg = error instanceof Error ? error.message : 'Error desconocido'
+          throw new Error(msg)
+      } */
+      console.error("error:", error);
+      throw error;
     }
-}
+  };
